524eef672024954ab65e69b2491767eb6c294289
philipgreat <philip_chang@163.com>
2019-01-10 00:02:19 +0800

    Regenerated microservices code and tested

--- bizui/src/bizcomponents/secuser/SecUser.associateform.js
+++ bizui/src/bizcomponents/secuser/SecUser.associateform.js
@@ -23,8 +23,8 @@ const testValues = {
   email: '',
   pwd: 'C183EC89F92A462CF45B95504792EC4625E847C90536EEFE512D1C9DB8602E95',
   verificationCode: '0',
-  verificationCodeExpire: '2018-12-28 09:33:32',
-  lastLoginTime: '2018-12-25 07:01:12',
+  verificationCodeExpire: '2019-01-07 11:11:32',
+  lastLoginTime: '2018-12-29 22:03:57',
   domainId: 'UD000001',
 }
 */
--- bizui/src/bizcomponents/secuser/SecUser.createform.js
+++ bizui/src/bizcomponents/secuser/SecUser.createform.js
@@ -22,8 +22,8 @@ const testValues = {
   email: '',
   pwd: 'C183EC89F92A462CF45B95504792EC4625E847C90536EEFE512D1C9DB8602E95',
   verificationCode: '0',
-  verificationCodeExpire: '2019-01-02 12:28:04',
-  lastLoginTime: '2018-12-21 23:32:33',
+  verificationCodeExpire: '2019-01-06 17:17:15',
+  lastLoginTime: '2018-12-27 03:00:25',
   domainId: 'UD000001',
 }
 */
