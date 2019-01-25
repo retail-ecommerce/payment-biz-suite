3cdece2922deb9ca2b7bfe3db8295e55fb8ebeb4
philipgreat <philip_chang@163.com>
2019-01-26 00:03:28 +0800

    Update to new version

--- bizcore/WEB-INF/payment_core_src/com/doublechaintech/payment/BaseViewPage.java
+++ bizcore/WEB-INF/payment_core_src/com/doublechaintech/payment/BaseViewPage.java
@@ -233,7 +233,11 @@ public abstract class BaseViewPage extends HashMap<String, Object> {
 		if (object instanceof FilterTabsViewComponent) {
 			return new FilterTabsSerializer();
 		}
-
+		/*
+		if (object instanceof BasePaymentFormProcessor) {
+			return new FormProcessorSerializer();
+		}
+		*/
 		if (object instanceof ButtonViewComponent) {
 			return new ButtonViewComponentSerializer();
 		}
